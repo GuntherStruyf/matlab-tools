e9f761dacf103c9822c45e863f0d828a07b593dc
Gunther Struyf <gunther.struyf@gmail.com>
2012-06-21 01:31:23 +0200

    simplified isInt

--- isInt.m
+++ isInt.m
@@ -1,7 +1,3 @@
 function retval = isInt(val)
-	if length(val)==1
-		retval = isnumeric(val) && isreal(val) && isfinite(val) && (val == fix(val));
-	else
-		retval = false;
-	end
+	retval = isscalar(val) && isnumeric(val) && isreal(val) && isfinite(val) && (val == fix(val));
 end
