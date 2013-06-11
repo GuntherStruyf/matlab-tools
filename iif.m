30cab3ae2903f1a0959d576d6f0f6e256d90b1a2
Gunther Struyf <gunther.struyf@gmail.com>
2013-06-11 22:44:44 +0200

    made iif vector compatible

--- iif.m
+++ iif.m
@@ -1,11 +1,15 @@
 function val = iif(expr, truepart, falsepart)
-	if isscalar(truepart)
-		truepart=truepart(ones(size(expr)));
+	if isscalar(expr)
+		val = iif_scalar(expr,truepart,falsepart);
+	else
+		if isscalar(truepart)
+			truepart=truepart(ones(size(expr)));
+		end
+		if isscalar(falsepart)
+			falsepart=falsepart(ones(size(expr))); 
+		end
+		val = arrayfun(@iif_scalar, expr, truepart, falsepart);
 	end
-	if isscalar(falsepart)
-		falsepart=falsepart(ones(size(expr))); 
-	end
-	val = arrayfun(@iif_scalar, expr, truepart, falsepart);
 end
 function val = iif_scalar(expr,truepart,falsepart)
 	if expr
